d173f81cceb79463a32200ea2cf231ede97e1a8e
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-04-22 23:16:04 +0530

    Update node.js

--- node.js
+++ node.js
@@ -29,3 +29,26 @@ app.get('/' , function(req , res){
 
 
 app.listen(3000);
+
+//erroe handling 
+
+const express = require ('express')
+const app = express();
+
+app.use(function(req, res , next){
+    console.log("midhhdlee chlaa")
+    next();
+});
+
+app.get('/' , function(req , res, next){
+   return next(new Error("erorr h"))  ;
+})
+
+app.use((err, req, res , next) => {
+    console.log(err.stack)
+    res.status(500).send('aailaa errorr')
+    
+})
+
+
+app.listen(3000);
